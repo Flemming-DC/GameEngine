8052cbcaa77c757fe37f9a2ca8df30a23daf0e44
Flemming-DC <flem1431@hotmail.com>
2024-02-27 23:43:47 +0100

    m

--- Engine/src/Diverse/Input.cpp
+++ Engine/src/Diverse/Input.cpp
@@ -4,8 +4,8 @@
 #include <GLFW/glfw3.h>
 
 static GLFWwindow* window;
-bool Input::scrollHeldDown = false;
-int Input::scrollDirection = 0;
+bool Input::glwfScrollHeldDown = false;
+int Input::glwfScrollDirection = 0;
 
 void Input::Setup(GLFWwindow* window_)
 {
@@ -19,8 +19,8 @@ void Input::Setup(GLFWwindow* window_)
 void Input::Update()
 {
     // resetting input data
-    scrollHeldDown = false;
-    scrollDirection = 0;
+    glwfScrollHeldDown = false;
+    glwfScrollDirection = 0;
 }
 
 
@@ -41,8 +41,8 @@ void Input::KeyboardCallback(GLFWwindow* window, int key, int scancode, int acti
 
 void Input::ScrollCallback(GLFWwindow* window, double pressed, double direction)
 {
-    scrollHeldDown = (pressed == 1);
-    scrollDirection = (int)direction;
+    glwfScrollHeldDown = (pressed == 1);
+    glwfScrollDirection = (int)direction;
 }
 
 bool Input::KeyHeldDown(Keyboard key)
@@ -57,40 +57,18 @@ bool Input::KeyHeldDown(Keyboard key)
 }
 
 
-ImGuiKey Input::GetImGuiKey(int glfwKey) 
-{
-    switch (glfwKey)
-    {
-    case GLFW_KEY_SPACE: return ImGuiKey_Space;
-    case GLFW_KEY_A: return ImGuiKey_A;
-    case GLFW_KEY_B: return ImGuiKey_B;
-    case GLFW_KEY_C: return ImGuiKey_C;
-    case GLFW_KEY_D: return ImGuiKey_D;
-    case GLFW_KEY_E: return ImGuiKey_E;
-    case GLFW_KEY_F: return ImGuiKey_F;
-    case GLFW_KEY_G: return ImGuiKey_G;
-    case GLFW_KEY_H: return ImGuiKey_H;
-    case GLFW_KEY_I: return ImGuiKey_I;
-    case GLFW_KEY_J: return ImGuiKey_J;
-    case GLFW_KEY_K: return ImGuiKey_K;
-    case GLFW_KEY_L: return ImGuiKey_L;
-    case GLFW_KEY_M: return ImGuiKey_M;
-    case GLFW_KEY_N: return ImGuiKey_N;
-    case GLFW_KEY_O: return ImGuiKey_O;
-    case GLFW_KEY_P: return ImGuiKey_P;
-    case GLFW_KEY_Q: return ImGuiKey_Q;
-    case GLFW_KEY_R: return ImGuiKey_R;
-    case GLFW_KEY_S: return ImGuiKey_S;
-    case GLFW_KEY_T: return ImGuiKey_T;
-    case GLFW_KEY_U: return ImGuiKey_U;
-    case GLFW_KEY_V: return ImGuiKey_V;
-    case GLFW_KEY_W: return ImGuiKey_W;
-    case GLFW_KEY_X: return ImGuiKey_X;
-    case GLFW_KEY_Y: return ImGuiKey_Y;
-    case GLFW_KEY_Z: return ImGuiKey_Z;
-    //case GLFW_KEY_: return ImGuiKey_;
-    default:
-        RaiseError("Missing key");
-        return ImGuiKey_COUNT; // Unknown key
-    }
+bool Input::GetScrollHeldDown()
+{ 
+    if (ImGui::GetIO().WantCaptureMouse)
+        return ImGui::IsKeyDown(ImGuiKey_MouseMiddle);
+    else
+        return glwfScrollHeldDown; 
+}
+
+int Input::GetScrollDirection()
+{ 
+    if (ImGui::GetIO().WantCaptureMouse)
+        return (int)ImGui::GetIO().MouseWheel;
+    else
+        return glwfScrollDirection; 
 }
--- Engine/src/Diverse/Input.h
+++ Engine/src/Diverse/Input.h
@@ -10,13 +10,13 @@ class Input
 public:
 	static void Setup(GLFWwindow* window_);
 	static bool KeyHeldDown(Keyboard key);
-	inline static bool GetScrollHeldDown() { return scrollHeldDown; }
-	inline static int GetScrollDirection() { return scrollDirection; }
+	static bool GetScrollHeldDown();
+	static int GetScrollDirection();
 	static void Update();
 	
 private:
-	static bool scrollHeldDown;  // currently unused by engine
-	static int scrollDirection; // 1 = forward, -1 = backwards, 0 = no scroll
+	static bool glwfScrollHeldDown;  // currently unused by engine
+	static int glwfScrollDirection; // 1 = forward, -1 = backwards, 0 = no scroll
 
 	static void LogFocusChange(GLFWwindow* window, int focused);
 	static void KeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
