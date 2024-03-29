f97ac145d6a5cf18e9f59949608fb4e505161bc5
Flemming-DC <flem1431@hotmail.com>
2024-03-29 12:50:26 +0100

    m

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -11,30 +11,9 @@
 //Scene::GetActiveScene().PurelyManualSetup();
 //Scene::GetActiveScene().Save();
 
-#include <iostream>
-
-template <typename... >
-using void_t = void;
-
-template <typename T, typename = void>
-struct is_const_callable_method : std::false_type {};
-
-template <typename T>
-struct is_const_callable_method<T, void_t<
-    decltype(std::declval<const T&>().to_string())
-    >> : std::true_type {};
-
-
-int _main()
-{
-    std::cout << is_const_callable_method<InputAction<bool>>::value << std::endl;
-    std::cout << is_const_callable_method<Entity>::value << std::endl;
-    return 0;
-}
 
 int main()
 {
-    _main();
     //logger::print(UuidCreator::MakeID());
     Dynamic::Setup<Editor, GameAssets, GameInputs>();
     Core::Run<DemoScene>();
